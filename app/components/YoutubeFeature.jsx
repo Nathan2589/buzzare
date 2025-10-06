1e908b26162a9ac918ff6ca56eb1256dd1e859a0
Nathan Dobbyn <nathandobbyn@gmail.com>
2025-10-06 16:34:13 +0100

    updated youtube feature

--- app/components/YoutubeFeature.jsx
+++ app/components/YoutubeFeature.jsx
@@ -28,7 +28,7 @@ export default function YoutubeFeature() {
           transition={{ duration: 0.8 }}
         >
           <div className="relative aspect-video">
-            <iframe width="900" height="500" src="https://www.youtube.com/embed/kSYN8Ez1YpY?si=_a1f0NDp7nOj9F34" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/eaeInlJwQcM?si=3QtfaPlnf1pk2eLZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
           
           <div className="p-6">
